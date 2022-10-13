874434d6a463a4aaddf97b16ccd47deb542156c6
wenchieh <alwjlola@gmail.com>
2022-10-14 00:30:59 +0800

    feat: task4 tree api

--- src/main/kotlin/task_4/DeleteLogInvokeTreeApi.kt
+++ src/main/kotlin/task_4/DeleteLogInvokeTreeApi.kt
@@ -5,6 +5,7 @@ import common.toFile
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.Opcodes
+import org.objectweb.asm.tree.AbstractInsnNode
 import org.objectweb.asm.tree.ClassNode
 import org.objectweb.asm.tree.MethodInsnNode
 
@@ -17,22 +18,25 @@ private fun deleteLogInvokeByTreeApi() {
     val classNode = ClassNode(Opcodes.ASM9)
     classReader.accept(classNode, ClassReader.SKIP_DEBUG)
 
+    //    remove
+    //    GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
+    //    ALOAD 1
+    //    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/Object;)V
     classNode.name = "sample/DeleteLogInvokeTreeClass"
     classNode.methods.forEach { methodNode ->
-        // 删除 System.out.println(); 调用
-        // TODO: 删除残余的局部变量 out 以及字符串变量
-        methodNode.instructions.filterIsInstance<MethodInsnNode>().filter {
-            it.name == "println" && it.desc == "(Ljava/lang/String;)V" && it.owner == "java/io/PrintStream"
-        }.forEach {
+        val deleteNodes = mutableListOf<AbstractInsnNode>()
+        methodNode.instructions.forEachIndexed { index, node ->
+            if (node is MethodInsnNode && node.opcode == Opcodes.INVOKEVIRTUAL && node.name == "println" && node.desc == "(Ljava/lang/String;)V" && node.owner == "java/io/PrintStream") {
+                deleteNodes.add(methodNode.instructions[index])
+                deleteNodes.add(methodNode.instructions[index - 1])
+                deleteNodes.add(methodNode.instructions[index - 2])
+            }
+        }
+
+        deleteNodes.forEach {
             methodNode.instructions.remove(it)
         }
-        // TODO: 不能按照下面方式删 out 变量
-//        methodNode.instructions.filterIsInstance<FieldInsnNode>().filter {
-//            it.name == "out" && it.desc == "Ljava/io/PrintStream;" && it.owner == "java/lang/System"
-//        }.forEach {
-//            println("${it.name} ${it.desc} ${it.owner}")
-//            methodNode.instructions.remove(it)
-//        }
+
     }
 
     val classWriter = ClassWriter(ClassWriter.COMPUTE_FRAMES)
